cba22ff678e5c4553fcee4bc0a70bd95abb196e5
es1210 <dmstn27034@gmail.com>
2024-08-16 02:53:26 +0900

    myBatis 추가, db 스키마 추가, mysql driver 추가, CRUD를 mybatis로 구현

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -1,14 +1,10 @@
 package com.example.demo.service;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.repository.ArticleRepository;
 import com.example.demo.vo.Article;
-
 @Service
 public class ArticleService {
 	@Autowired
@@ -16,41 +12,25 @@ public class ArticleService {
 
 	public ArticleService(ArticleRepository articleRepository) {
 		this.articleRepository = articleRepository;
-		makeTestData();
+	
 	}
 
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-
-			articleRepository.writeArticle(title, body);
-		}
+	
+	public int writeArticle(String title, String body) {
+		articleRepository.writeArticle(title, body);
+		return articleRepository.getLastInsertId();
 	}
-
-	public Article writeArticle(String title, String body) {
-
-		return articleRepository.writeArticle(title, body);
-	}
-
 
 	public void deleteArticle(int id) {
 		articleRepository.deleteArticle(id);
 	}
-
 	public void modifyArticle(int id, String title, String body) {
-
 		articleRepository.modifyArticle(id, title, body);
 	}
-
-
 	public Article getArticleById(int id) {
-
 		return articleRepository.getArticleById(id);
 	}
-
 	public List<Article> getArticles() {
 		return articleRepository.getArticles();
 	}
-
 }
